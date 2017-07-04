3001ed0b92bad1fd4ecaa9908f8efa42bc673964
umra_st <stefan.umrath@dlr.de>
2017-07-04 07:29:23 +0200

    implementation of Wittmans translation coefficients for translations in z-direction

--- code/my_vsh_rotations.py
+++ code/my_vsh_rotations.py
@@ -6,6 +6,7 @@ from math import sqrt
 import quaternion
 import spherical_functions as sf
 from scipy.special import jv, hankel1
+from cython_gaunt import gaunt
 
 jn = lambda l, x: jv(l + 0.5, x)*np.sqrt(np.pi/(2*x))
 h1 = lambda l, x. hankel1(l + 0.5, x)*np.sqrt(np.pi/(2*x))
@@ -47,6 +48,30 @@ class HarmonicField:
                 self.ncoeffs[l**2 + l - 1 + m] = (
                         self.ncoeffs[istart:istop] 
                          * D[istart:istop]).sum()
+
+    def Vl1l2_m(self, l1, l2, m, kd, sign_z):
+        """return the (l1, m) contribution of a vector spherical 
+           harmonic M_l1,m (N_l1,m) when it is translated along (sign_z = +1)
+           or against (sign_z = -1) the z-direction an adimensional distance
+           kd. 
+           Returns the polarization conserving contribution M_l1,m (N_l1,m) 
+           as the first and the polarization mixing contribution N_l1,m (M_l1,m)
+           as the second value
+        """
+        
+        common =  ((-1)**m*4*np.pi * 1j**(l2-l1)/
+                   sqrt(l2*(l2+1) * l1*(l1+1)))
+        alpha = np.arange(abs(l1-l2), l1+l2+1, 2)
+        gaunts = gaunt(l1, l2, m)
+        h1_sph= hankel1(alpha+0.5, kd)*math.sqrt(np.pi/(2*kd))
+        fak_PP = ((l1*(l1+1) + l2*(l2+1) - alpha*(alpha+1))*0.5 * h1_sph *
+                    (sign_z*1j)**alpha*gaunts*np.sqrt((2*alpha+1)/(4*np.pi))).sum()
+        fak_PQ = (-m*kd*(1j)**(alpha+1) * gaunts * np.sqrt((2*alpha+1)/(4*np.pi))
+                  *h1_sph * sign_z**(alpha+1)).sum()
+        PP = fak_PP * common
+        PQ = fak_PQ * common
+        return PP, PQ
+
     def z_translate(self, kd):
         """ calculate the VSH-coefficients in a frame of 
             reference which is translated kd/k along the 
@@ -55,11 +80,9 @@ class HarmonicField:
             R. C. Wittmann, IEEE Trans. Antennas Propag. 34 (1988)
         """
         l = np.arange(1, self.lmax).reshape(self.lmax, 1)
-        nu = l.transpose()
-        common = lambda nu, l, m: ((-1)**m*4*np.pi*1j**(l-nu)/
-                                    sqrt(l*(l+1)*nu*(nu+1))
-                                  )
-        for 
+        
+        for l in range(1, self.lmax+1):
+            for 
         
 
 if __name__ == "__main__":
