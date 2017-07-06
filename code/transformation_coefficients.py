f5ee526644ebda4c52a74a87a744dadac662c2ec
umra_st <stefan.umrath@dlr.de>
2017-07-06 11:35:29 +0200

    update

--- code/transformation_coefficients.py
+++ code/transformation_coefficients.py
@@ -3,19 +3,18 @@ import spherical_functions as sf
 import quaternion
 from cython_gaunt import gaunt
 from scipy.special import hankel1, jv
+from math import sqrt, pi
 
 
 def WignerD_matrices(l, theta_y, phi_z):
     rotor = quaternion.from_spherical_coords(theta_y, phi_z)
     D = sf.WignerD.Wigner_D_matrices(rotor, 1, l)
     D1 = D[0:9]
-    print(D1)
     result = [np.array([D1[3*i:(i+1)*3] for i in range(3)], dtype = np.complex128)]
     for alpha in range(2, l + 1):
         start = int((4*alpha**3 - alpha - 3)/3)
         line = 2*alpha + 1
         Dl = D[start:start+line**2]
-        print("shape D_l=%i: " %alpha, np.shape(Dl))
         for j in range(1, line):
             Matrix = np.array([Dl[j*line:(j+1)*line] for j in range(line)], 
                                 dtype = np.complex128)
@@ -36,19 +35,20 @@ def translate_l1l2(l1, l2, m, kd, sign_z, regreg):
     if abs(m) > max(l1, l2):
         raise ValueError("translate_l1l2 should not be called \
         with |m| > max(l1, l2), but got l1=%i, l2=%i, m=%i" %(l1,l2,m))
-    common =  ((-1)**m*1j**(l1-l2)/   
-               np.sqrt(l2*(l2+1) * l1*(l1+1)/(np.pi*4)))
+    common =  ((-1)**m * (sign_z*1j)**(l1-l2)*2*   
+               sqrt(pi/(l1*(l1+1)*l2*(l2+1))))
     alpha = np.arange(abs(l1-l2), l1+l2+1, 2)
     gaunts = gaunt(l1, l2, m)
-    spherical_factor = np.sqrt(np.pi/(2*kd))
+    spherical_factor = sqrt(pi/(2*kd))
     if regreg:
-        sphB = jv(alpha+0.5, kd)*spherical_factor
+        bessel = jv(alpha+0.5, kd)
     else: 
-        sphB = hankel1(alpha+0.5, kd)*spherical_factor
-    fak_PP = ((l1*(l1+1) + l2*(l2+1) - alpha*(alpha+1))*0.5 * sphB *
-                 (sign_z*1j)**alpha*gaunts*np.sqrt(2*alpha+1)).sum()
-    fak_PQ = (-m*kd*(1j)**(alpha+1) * gaunts * np.sqrt(2*alpha+1)
-              *sphB * sign_z**(alpha)).sum()
+        bessel = hankel1(alpha+0.5, kd)
+    common_alpha_factor = (np.sqrt((2*alpha+1)*pi/(2*kd)) * 
+                    1j**alpha * bessel * gaunts)
+    fak_PP = ((l1*(l1+1) + l2*(l2+1) - alpha*(alpha+1)) * 0.5 
+               * common_alpha_factor).sum()
+    fak_PQ = (-m*1j*kd * common_alpha_factor).sum()
     PP = fak_PP * common
     PQ = fak_PQ * common
     return [PP, PQ]
@@ -66,6 +66,10 @@ def translation_matrix(l1_max, l2_max, m, kd, sign_z, regreg):
        since the result does not depend upon the sign of m 
        it should be not recalculated if needed for m = -lmax..+lmax
     """
+    transpose = False
+    if l1_max > l2_max:
+        transpose = True
+        l1_max, l2_max = l2_max, l1_max
     smaller_l = min(l1_max, l2_max)
     lmin = max(abs(m), 1)
     dim1 = l1_max - lmin + 1
@@ -83,12 +87,15 @@ def translation_matrix(l1_max, l2_max, m, kd, sign_z, regreg):
             result[l1-lmin+Di, l2-lmin+Dj] = PP
             result[l1-lmin, l2-lmin+Dj] = PQ
             result[l1-lmin+Di, l2-lmin] = PQ
-            if ([l2-lmin+1, l1-lmin+1] <= [result.shape[0]/2,
+            l1l2_switch = -1j*sign_z
+            if ([l2-lmin+1+Dj, l1-lmin+1+Di] <= [result.shape[0]/2,
                         result.shape[1]/2] and not l1 == l2):
-                result[l2-lmin, l1-lmin] = -1j*PP
-                result[l2-lmin+Dj, l1-lmin+Di] = -1j*PP
-                result[l2-lmin+Dj, l1-lmin] = -1j*PQ 
-                result[l2-lmin, l1-lmin+Di] = -1j*PQ
+                result[l2-lmin, l1-lmin] = l1l2_switch*PP
+                result[l2-lmin+Dj, l1-lmin+Di] = l1l2_switch*PP
+                result[l2-lmin+Dj, l1-lmin] = l1l2_switch*PQ 
+                result[l2-lmin, l1-lmin+Di] = l1l2_switch*PQ
+    if transpose:
+        result.transpose()
     return result
             
 
