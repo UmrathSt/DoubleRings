sign error correct in Useful_identities polarization mixing coefficient +-

code/MultipoleFields.py
@@ -7,7 +7,7 @@ from MultipoleTransformations import WignerD_matrices as WignerDs
 from MultipoleTransformations import translation_matrix as translations
 from MultipoleTransformations  import translation_matrix_debug as translationsD
 from MultipoleTransformations import full_translation_matrix as Tfull
-
+from MultipoleTransformations import full_rotation_matrix as Rfull
 
 class HarmonicField:
     def __init__(self, coeffs, lmax):
@@ -69,6 +69,14 @@ class HarmonicField:
             self.coeffs[i0:i1] = np.dot(Ds[l-1], self.coeffs[i0:i1])
             self.coeffs[i0+jump:i1+jump] = np.dot(Ds[l-1], 
                                 self.coeffs[i0+jump:i1+jump])
+    def rotate_matrix(self, theta, phi):
+        """ applies a rotation by multiplication of the 
+            multipole-coefficients with the full rotation 
+            matrix
+        """
+        R = Rfull(self.lmax, theta, phi)
+        self.coeffs = np.dot(R, self.coeffs)
+            
     def z_translate_matrix(self, kd, sign_z, regreg, debug = False):
         """ do a translation and use matrixmultiplication by 
             the full translation matrix
@@ -105,17 +113,19 @@ class HarmonicField:
         self.coeffs[m0_idx, 0] = np.dot(Tmatrix, self.coeffs[m0_idx])[0,0]
 
 if __name__ == "__main__":
-    np.set_printoptions(precision=3)
-    lmax = 5 
+    np.set_printoptions(precision=3, suppress=True)
+    lmax = 50 
     ncoeffs = 2*lmax*(lmax+2)
     m = np.zeros(ncoeffs, dtype = np.complex128).reshape(ncoeffs, 1)
     l1_coeffs =  np.array([-1, 0.01, 1, -20, -10, 0.1, 10, 20], dtype = np.complex128)
     m[:l1_coeffs.size,0] = l1_coeffs
+    offset = lmax*(lmax+2)
+    m[offset:offset+l1_coeffs.size,0] = 1j*l1_coeffs
     coeffs = m.copy()
     field = HarmonicField(coeffs, lmax)
     print("Koeffizienten")
     print(field.coeffs)
-    kd = 0.5
+    kd = 5
     field.z_translate_matrix(kd, sign_z = 1, regreg = 1)
     field.z_translate_matrix(kd, sign_z =-1, regreg = 1)
     field.reduce_lmax(2)

code/MultipoleTransformations.py
@@ -183,12 +183,12 @@ def translation_matrix_debug(l1_max, l2_max, m, kd, sign_z, regreg):
 
 if __name__ == "__main__":
     #print(WignerD_matrices(2, 0, 1.5))
-    l1_max, l2_max = 7, 10 
-    m, kd = 5, 0.5
+    l1_max, l2_max = 20, 10 
+    m, kd = 5, 5
     debug = True
     sign_z, regreg = 1, 1
-    T1 = translation_matrix_debug(l1_max, l2_max, m, kd, +sign_z, 
+    T1 = full_translation_matrix(l1_max, l1_max, kd, +sign_z, 
             regreg)
-    T2 = translation_matrix_debug(l2_max, l1_max, m, kd, -sign_z, 
+    T2 = full_translation_matrix(l1_max, l1_max, kd, -sign_z, 
             regreg )
     print(np.dot(T1, T2))

code/tests/test_MultipoleFields.py
@@ -41,17 +41,22 @@ class MultipoleFields_coefficients(unittest.TestCase):
             z-direction is the same as a field rotated by pi around the
             y-axis and then translated a distance -d in the z-direction
         """
-        lmax = 5
-        kd = 2
+        lmax = 15
+        lmax_red = 5
+        kd = 1 
         sign_z = 1
         regreg = 1
         coeffs = np.arange(lmax*(lmax + 2)*2)[:,np.newaxis]
-        hf1 = HarmonicField(coeffs.copy(), lmax)
-        hf1.z_translate(kd, sign_z, regreg)
-        hf2 = HarmonicField(coeffs.copy(), lmax)
-        hf2.rotate(np.pi, 0)
-        hf2.z_translate(kd, -sign_z, regreg)
-        hf2.rotate(-np.pi, 0)
+        coeffs1 = coeffs.copy()
+        coeffs2 = coeffs.copy()
+        hf1 = HarmonicField(coeffs1, lmax)
+        hf1.z_translate_matrix(kd, sign_z, regreg)
+        hf1.reduce_lmax(lmax_red)
+        hf2 = HarmonicField(coeffs2, lmax)
+        hf2.rotate_matrix(np.pi, 0)
+        hf2.z_translate_matrix(kd, -sign_z, regreg)
+        hf2.rotate_matrix(-np.pi, 0)
+        hf2.reduce_lmax(lmax_red)
         assert_array_almost_equal(hf1.coeffs, hf2.coeffs, decimal=4)
 if __name__ == "__main__":
     unittest.main()

code/tests/test_translations.py
@@ -7,7 +7,7 @@ from numpy.testing import assert_array_almost_equal
 import unittest
 from MultipoleTransformations import translation_matrix, translate_l1l2
 from MultipoleTransformations import translation_matrix_debug
-
+from MultipoleTransformations import full_translation_matrix
 
 class Translation_functionality(unittest.TestCase):
     """ Run functionality tests
@@ -57,23 +57,23 @@ class Translation_functionality(unittest.TestCase):
         l2max = self.l2max + 5
         m = 1 
         dim = 2*(l1max - max(abs(m), 1) + 1)
-        T = translation_matrix(l1max, l2max, m, d, sign_z, regreg)
-        Ti= translation_matrix(l2max, l1max, m, d, -sign_z, regreg)
-        assert_array_almost_equal(np.dot(T, Ti), np.eye(dim), decimal=3)
+        T = full_translation_matrix(l2max, l2max, d, sign_z, regreg)
+        Ti= full_translation_matrix(l2max, l2max, d, -sign_z, regreg)
+        dot = np.dot(T, Ti)
+        assert_array_almost_equal(dot, np.eye(dot.shape[0]), decimal=3)
 
     def test_double_pos_translation(self):
         """ Test if a translation of distance d, twice, is the same
             as a single translation of a distance 2*d
         """
         l1max = self.l1max
-        l2max = self.l2max
-        m = 2
+        l2max = self.l1max
         d = 0.51
         regreg = self.regreg
         sign_z = 1
-        T1d = translation_matrix(l1max, l2max, m, d, sign_z, regreg)
-        T2d = translation_matrix(l2max, l1max, m, d, sign_z, regreg)
-        Tdd = translation_matrix(l1max, l1max, m, 2*d, sign_z, regreg)
+        T1d = full_translation_matrix(l1max, l2max, d, sign_z, regreg)
+        T2d = full_translation_matrix(l2max, l1max, d, sign_z, regreg)
+        Tdd = full_translation_matrix(l1max, l1max, 2*d, sign_z, regreg)
         Tdot = np.dot(T1d, T2d)
         Di = l1max 
         Dj = l2max