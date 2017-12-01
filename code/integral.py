9d51634e292b41d35857ca2c4e9d5ddd00b52ed9
UmrathSt <Stefan.Umrath@gmx.net>
2017-12-01 08:16:45 +0100

    Tmatrices of perfectly conducting thin rings are now computed exactly and coincide with numerical results of scuff-em

--- code/integral.py
+++ code/integral.py
@@ -2,30 +2,48 @@ from legendre import jl, hl, Nlm, jdl, hdl, diffPlm, Plm
 import numpy as np
 
 def b_correction(kR, l1, m1, lmax):
+    if not (l1+m1)%2:
+        return 0
     denom =   (Nlm(l1, m1)*diffPlm(l1, m1))**2*hl(l1,kR)*jl(l1,kR)
     factor = 1
     ls = np.arange(max(1,abs(m1)), lmax+1)
-    l_e = ls[1::2]
-    l_o = ls[::2]
-    factor += np.array([(Nlm(l, m1)*diffPlm(l, m1))**2*hl(l, kR)*jl(l,kR) for l in l_o]).sum()
-    factor += np.array([(Nlm(l, m1)*Plm(l, m1)/kR*m1)**2*hdl(l, kR)*jdl(l,kR) for l in l_e]).sum()
+    factor += np.array([(Nlm(l, m1)*diffPlm(l, m1))**2*hl(l, kR)*jl(l,kR) for l in ls]).sum()
+    factor += np.array([(Nlm(l, m1)*Plm(l, m1)/kR*m1)**2*hdl(l, kR)*jdl(l,kR) for l in ls]).sum()
+    factor /= denom
+    return 1/factor
+
+def a_correction(kR, l1, m1, lmax):
+    if (l1+m1)%2 or m1 == 0:
+        return 0
+    denom = (Nlm(l1, m1)*Plm(l1, m1)/kR*m1)**2*hdl(l1,kR)*jdl(l1,kR)
+    factor = 1
+    ls = np.arange(max(1,abs(m1)), lmax+1)
+    factor += np.array([(Nlm(l, m1)*diffPlm(l, m1))**2*hl(l, kR)*jl(l,kR) for l in ls]).sum()
+    factor += np.array([(Nlm(l, m1)*Plm(l, m1)/kR*m1)**2*hdl(l, kR)*jdl(l,kR) for l in ls]).sum()
     factor /= denom
     return 1/factor
 
 
-
 if __name__ == "__main__":
-    LMAX = 200
-    kr = 1 
-    m1 = 0 
-    l1 = 1 
     from matplotlib import pyplot as plt
-    l = np.arange(1, LMAX+1)
-    print(l)
-    result = np.zeros(LMAX, dtype=np.complex128)
-    for lmax in l:
-        print("filling idx ", lmax-1)
-        result[lmax-1] = b_correction(kr, l1, m1, lmax)
-    plt.plot(l, np.abs(result), "ko")
-    print(result)
+    plt.rc("text", usetex=True)
+    LMAX = 100
+    lmax = 21
+    kr = 0.1 
+    mTE, mTM = 3, 3 
+    l = np.arange(1, lmax+1)
+    result_a = np.zeros(lmax, dtype=np.complex128)
+    result_b = np.zeros(lmax, dtype=np.complex128)
+
+    for L in l:
+        result_a[L-1] = a_correction(kr, L, mTM, LMAX) 
+        result_b[L-1] = b_correction(kr, L, mTE, LMAX) 
+    plt.plot(l, np.abs(result_a), linestyle="", markeredgewidth="1.5", markersize=8,
+            label="$a_\ell$", markeredgecolor="r", marker="o", markerfacecolor="None")
+    plt.plot(l, np.abs(result_b), linestyle="", markeredgewidth="1.5", markersize=8,
+            label="$b_\ell$", markeredgecolor="b", marker="x", markerfacecolor="None")
+    plt.xlabel("$\ell$", fontsize=14)
+    plt.ylabel("$a^\mathrm{ring}_\ell/a^\mathrm{sphere}_\ell, \
+                 b^\mathrm{ring}_\ell/b^\mathrm{sphere}_\ell$", fontsize=14)
+    plt.legend(loc="best", fontsize=14).draw_frame(False)
     plt.show()
