8669841c5f16efe018e22050de5d2a7c926f7c2f
UmrathSt <Stefan.Umrath@gmx.net>
2018-10-11 09:34:09 +0200

    impedance extraction for FSS added

---
+++ Broadband_Absorber/python_scripts/impedance_extraction.py
@@ -0,0 +1,55 @@
+from matplotlib import pyplot as plt
+import argparse
+import numpy as np
+parser = argparse.ArgumentParser()
+parser.add_argument("--eps", dest="eps", type=float)
+parser.add_argument("--kappa", dest="kappa", type=float)
+parser.add_argument("--lz", dest="lz", type=float)
+parser.add_argument("--fmin", dest="fmin", type=int, default=0)
+parser.add_argument("--fmax", dest="fmax", type=int, default=1e18)
+parser.add_argument("--filename", dest="filename", type=str)
+
+
+def get_Zsubs(eps,d,f):
+    """ Return the impedance of a dielectric slab
+        with permittivity eps, thickness d at frequency
+        f
+    """
+    w = 2*np.pi*f
+    return 1j*Z0/np.sqrt(eps)*np.tan(w*np.sqrt(eps)/C0)
+
+def get_Zfss(Z0, Zd, Gamma0):
+    """ Return the impedance of a frequency selective surface
+        which is modelled in parallel to an impedance Zsubs
+    """
+    r = (1-Gamma0)/(1+Gamma0)
+    return Z0*Zd/(r*Zd-Z0) 
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    kappa = args.kappa
+    d = np.loadtxt(args.filename, delimiter=",")
+    f, Gamma0 = d[:,0], d[:,1]+1j*d[:,2]
+    fmin, fmax = args.fmin, args.fmax
+    i_min = (np.abs(f-fmin)).argmin() 
+    i_max = (np.abs(f-fmax)).argmin() 
+    f = f[i_min:i_max]
+    Gamma0 = Gamma0[i_min:i_max]
+    Z0 = 376.73
+    C0 = 299792458
+    EPS0 = 8.85e-12
+    eps = args.eps
+    lz = args.lz
+    eps = eps+kappa*1j/(2*np.pi*f*EPS0)
+    Zd = get_Zsubs(eps, args.lz, f)
+    Zfss = get_Zfss(Z0, Zd, Gamma0)
+    plt.plot(f/1e9, np.real(Zfss),"r-",linewidth=2,
+            label="$\mathcal{R}(Z_\mathrm{fss})$")
+    plt.plot(f/1e9, np.imag(Zfss),"b-",linewidth=2,
+            label="$\mathcal{I}(Z_\mathrm{fss})$")
+    plt.xlabel("f [GHz]")
+    plt.ylabel("$\mathcal{R}(Z_\mathrm{fss}),\mathcal{I}(Z_\mathrm{fss})$")
+    plt.xlim(f[[0,-1]]/1e9)
+    plt.legend(loc="best").draw_frame(False)
+    plt.show()
